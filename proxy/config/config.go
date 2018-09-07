0b4f38c77e0c42801fb809e4d2fc06180d58401b
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-09-08 00:01:33 +0200

    Lint

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -6,10 +6,10 @@ import (
 )
 
 const (
-	listenAddr     = "" // Listen to every address
-	listenPort     = "1234"
-	serverAddr     = "127.0.0.1"
-	serverPort     = "27016"
+	listenAddr    = "" // Listen to every address
+	listenPort    = "1234"
+	serverAddr    = "127.0.0.1"
+	serverPort    = "27016"
 	HandleTimeout = 5 * time.Second
 
 	ListenFullAddr = listenAddr + ":" + listenPort
