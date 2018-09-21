1a6f873df8ad061279b9d2c7c80de46bc390c375
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2018-09-21 23:28:56 +0200

    improve coverage of config.go

--- proxy/config/config.go
+++ proxy/config/config.go
@@ -1,10 +1,50 @@
 package config
 
 import (
-	"time"
 	"runtime"
 	"os"
+	"sync"
+	"github.com/golang/glog"
+	"time"
 )
+
+const (
+	HandleTimeout           = 5 * time.Second
+	ServerConnectionTimeout = 1 * time.Minute
+
+	defaultListenIP   = "0.0.0.0"
+	defaultListenPort = "27015"
+	defaultServerIP   = "192.168.0.2"
+	defaultServerPort = "27015"
+
+	listenIPKey   = "LISTEN_ADDR"
+	listenPortKey = "LISTEN_PORT"
+	serverIPKey   = "SERVER_ADDR"
+	serverPortKey = "SERVER_PORT"
+)
+
+var (
+	workerCount    = runtime.NumCPU()
+	listenFullAddr string
+	serverFullAddr string
+	once           sync.Once
+)
+
+const MaxDatagramSize = 4096
+
+func ListenAddr() string {
+	once.Do(extractConfFromEnvVars)
+	return listenFullAddr
+}
+
+func ServerAddr() string {
+	once.Do(extractConfFromEnvVars)
+	return serverFullAddr
+}
+
+func WorkerCount() int {
+	return workerCount
+}
 
 func getEnvOrDefault(envKey string, defaultValue string) string {
 	if v, ok := os.LookupEnv(envKey); ok {
@@ -14,29 +54,14 @@ func getEnvOrDefault(envKey string, defaultValue string) string {
 	}
 }
 
-var (
-	listenAddr              = getEnvOrDefault("LISTEN_ADDR", "")
-	listenPort              = getEnvOrDefault("LISTEN_PORT", "1234")
-	serverAddr              = getEnvOrDefault("SERVER_ADDR", "91.121.51.220")
-	serverPort              = getEnvOrDefault("SERVER_PORT", "27016")
-	HandleTimeout           = 5 * time.Second
-	ServerConnectionTimeout = 1 * time.Minute
-
-	workerCount    = runtime.NumCPU()
+func extractConfFromEnvVars() {
+	var (
+		listenAddr = getEnvOrDefault(listenIPKey, defaultListenIP)
+		listenPort = getEnvOrDefault(listenPortKey, defaultListenPort)
+		serverAddr = getEnvOrDefault(serverIPKey, defaultServerIP)
+		serverPort = getEnvOrDefault(serverPortKey, defaultServerPort)
+	)
+	glog.Info("Extracting configuration from environment variables.")
 	listenFullAddr = listenAddr + ":" + listenPort
 	serverFullAddr = serverAddr + ":" + serverPort
-)
-
-const MaxDatagramSize = 4096
-
-func ListenAddr() string {
-	return listenFullAddr
-}
-
-func ServerAddr() string {
-	return serverFullAddr
-}
-
-func WorkerCount() int {
-	return workerCount
 }
---
+++ proxy/config/config_test.go
@@ -0,0 +1,159 @@
+package config
+
+import (
+	"testing"
+	"os"
+	"runtime"
+)
+const (
+	testIP   = "1.2.3.4"
+	testPort = "7532"
+)
+
+func Test_getEnvOrDefault(t *testing.T) {
+	type args struct {
+		envKey       string
+		defaultValue string
+	}
+	const (
+		testValDefault = "ThisIsATest"
+		testValEnv     = "MyValue"
+		testKeyEnv     = "MyKey"
+	)
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "unknown_env_key",
+			args: args{
+				envKey:       "UNKNOWN_ENV_KEY",
+				defaultValue: testValDefault,
+			},
+			want: testValDefault,
+		},
+		{
+			name: "existing_env_key",
+			args: args{
+				envKey:       testKeyEnv,
+				defaultValue: testValEnv,
+			},
+			want: testValEnv,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Setenv(testKeyEnv, testValEnv)
+			if got := getEnvOrDefault(tt.args.envKey, tt.args.defaultValue); got != tt.want {
+				t.Errorf("getEnvOrDefault() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestServerAddr(t *testing.T) {
+	type envVars map[string]string
+
+	tests := []struct {
+		name string
+		env  envVars
+		want string
+	}{
+		{
+			name: "default_ip_and_port",
+			env:  envVars{},
+			want: defaultServerIP + ":" + defaultServerPort,
+		},
+		{
+			name: "default_port",
+			env: envVars{
+				serverIPKey: testIP,
+			},
+			want: testIP + ":" + defaultServerPort,
+		},
+		{
+			name: "default_ip",
+			env: envVars{
+				serverPortKey: testPort,
+			},
+			want: defaultServerIP + ":" + testPort,
+		},
+		{
+			name: "ok",
+			env: envVars{
+				serverIPKey:   testIP,
+				serverPortKey: testPort,
+			},
+			want: testIP + ":" + testPort,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Clearenv()
+			for k, v := range tt.env {
+				os.Setenv(k, v)
+			}
+			extractConfFromEnvVars() // force re-extraction
+			if got := ServerAddr(); got != tt.want {
+				t.Errorf("ServerAddr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestListenAddr(t *testing.T) {
+	type envVars map[string]string
+
+	tests := []struct {
+		name string
+		env  envVars
+		want string
+	}{
+		{
+			name: "default_ip_and_port",
+			env:  envVars{},
+			want: defaultListenIP + ":" + defaultListenPort,
+		},
+		{
+			name: "default_port",
+			env: envVars{
+				listenIPKey: testIP,
+			},
+			want: testIP + ":" + defaultListenPort,
+		},
+		{
+			name: "default_ip",
+			env: envVars{
+				listenPortKey: testPort,
+			},
+			want: defaultListenIP + ":" + testPort,
+		},
+		{
+			name: "ok",
+			env: envVars{
+				listenIPKey:   testIP,
+				listenPortKey: testPort,
+			},
+			want: testIP + ":" + testPort,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Clearenv()
+			for k, v := range tt.env {
+				os.Setenv(k, v)
+			}
+			extractConfFromEnvVars() // force re-extraction
+			if got := ListenAddr(); got != tt.want {
+				t.Errorf("ListenAddr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestWorkerCount(t *testing.T) {
+	want := runtime.NumCPU()
+	if got := WorkerCount(); got != want {
+		t.Errorf("WorkerCount() = %v, want %v", got, want)
+	}
+}
